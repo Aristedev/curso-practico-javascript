0e16f3d4efa5b595ab43065ca01c87aac4816bb1
ariste <ariste.devstart@gmail.com>
2021-10-06 18:02:05 -0500

    Se agrega la mediana del top 10 de los salarios

--- salarios/analisis.js
+++ salarios/analisis.js
@@ -1,20 +1,7 @@
 // SOLO EL ANALISIS DE LA LISTA DE PERSONAS EN salarios.js
 
-// FILTRAMOS LOS SALARIOS DE LAS PERSONAS
-const salariosPe = peru.map(
-    function (personita) {
-        return personita.salario;
-    }
-);
-
-// ORDENAMOS LOS SALARIOS, DE MENOR A MAYOR
-const salariosPeSort = salariosPe.sort(
-    function (salarioA, salarioB) {
-        return salarioA - salarioB;
-    }
-);
-
-// VERRIFICAMOS SI LA LISTA ES PAR O IMPAR
+// HELPERS O UTILS:
+//  * VERIFICAMOS SI LA LISTA ES PAR O IMPAR
 function esPar(numerito) {
     // if (numerito % 2 == 0) {
     //     return true;
@@ -25,7 +12,7 @@ function esPar(numerito) {
     return(numerito % 2 == 0);
 };
 
-// CALCULAMOS EL PROMEDIO DE LOS NUMEROS INGRESADOS
+//  * CALCULADORA DEL PROMEDIO DE LOS NUMEROS INGRESADOS
 function calcularMediaAritmetica(lista) {
     const sumaLista = lista.reduce(
         function (valorAcumulado = 0, nuevoVal) {
@@ -34,9 +21,11 @@ function calcularMediaAritmetica(lista) {
     )
     const promedioLista = sumaLista / lista.length;
     return promedioLista;
-}
+};
 
-// MEDIANA DE SALARIOS QUE RECIBE LA LISTA DE CUALQUIER PAÍS
+
+// CALCULADORA DE MEDIANA:
+//  * MEDIANA DE SALARIOS QUE RECIBE LA LISTA DE CUALQUIER PAÍS
 function medianaSalarios(lista) {
     const mitad = parseInt(lista.length / 2)
 
@@ -51,8 +40,46 @@ function medianaSalarios(lista) {
         const personitaMitad = lista[mitad];
         return personitaMitad;
     }
-}
+};
 
-console.log(
-    medianaSalarios(salariosPeSort)
+
+// MEDIANA GENERAL:
+// --------------------------------------------- DEBEN ESTAR EN ESTE ORDEN -> LAS CONSTANTES NO PRESENTAN HOISTING ----------------------
+//  * FILTRAMOS LOS SALARIOS DE LAS PERSONAS
+const salariosPe = peru.map(
+    function (personita) {
+        return personita.salario;
+    }
 );
+
+//  * ORDENAMOS LOS SALARIOS, DE MENOR A MAYOR
+const salariosPeSort = salariosPe.sort(
+    function (salarioA, salarioB) {
+        return salarioA - salarioB;
+    }
+);
+// -------------------------------------------------------------------------------------------------------------------------------------
+const medianaGeneralPe = medianaSalarios(salariosPeSort);
+
+
+// MEDIANA DEL TOP 10%:
+// El corte spliceStart debe empezar en el 90%
+// const spliceStart = 0;
+//spliceStart = (salariosPeSort.lenght * (100 - 10)) / 100;
+const spliceStart = (salariosPeSort.length * (90)) / 100;
+
+// El corte spliceCount debe ser el resto del porcentaje(hasta el final)
+// const spliceCount = 0;
+const spliceCount = salariosPeSort.length - spliceStart;
+
+const salariosPeTop10 = salariosPeSort.splice(
+    spliceStart,
+    spliceCount
+);
+
+const medianaTop10Pe = medianaSalarios(salariosPeTop10);
+
+console.log({
+    medianaGeneralPe,
+    medianaTop10Pe
+});
